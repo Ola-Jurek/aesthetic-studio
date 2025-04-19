7a7d4533d83981a678f9bd60f1683a6fa04cf8f1
Ola-Jurek <olajurek12@gmail.com>
2025-04-20 00:25:27 +0200

    mobileSlider

--- aesthetic-studio-vite-project/src/js/mobileSlider.jsx
+++ aesthetic-studio-vite-project/src/js/mobileSlider.jsx
@@ -9,7 +9,7 @@ import bartek from "../assets/bartek2.jpg";
 
 export function MobileSlider() {
     const photos = [team2, marta, team3, bartek];
-
+    const [fade, setFade] = useState(false);
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const nextSlide = () => {
@@ -17,64 +17,75 @@ export function MobileSlider() {
     };
 
     useEffect(() => {
-        const interval = setInterval(nextSlide, 7000);
+        const interval = setInterval(() => {
+            setFade(true);
+            setTimeout(() => {
+                setCurrentIndex((prev) => (prev + 1) % photos.length);
+                setFade(false);
+            }, 1000); // fade duration
+        }, 7000);
+
         return () => clearInterval(interval);
     }, []);
 
-    return (
-        <Col sm={12}>
-            <div className='slider-app-mobile'>
-                <div className='slider-container-app-mobile'>
-                    {photos.map((photo, index) => (
-                        <img
-                            key={index}
-                            src={photo}
-                            alt={`Slide ${index}`}
-                            className={`slider-image-app-mobile ${index === currentIndex ? 'fade-in' : ''}`}
-                            style={{
-                                opacity: index === currentIndex ? 1 : 0,
-                                transition: 'opacity 2s ease-in-out, z-index 0s 0.5s',
-                                position: 'absolute',
-                                width: '100%',
-                                zIndex: index === currentIndex ? 1 : 0,
-                            }}
-                        />
-                    ))}
+        return (
+            <Col sm={12}>
+                <div className='slider-app-mobile'>
+                    <div className='slider-container-app-mobile'>
+                        {/*{photos.map((photo, index) => (*/}
+                        {/*{prevIndex !== null && isTransitioning && (*/}
+                            <img
+                                src={photos[currentIndex]}
+                                alt={`Slide ${currentIndex}`}
+                                className="slider-image-app-mobile"
+                                // key={index}
+                                // src={photo}
+                                // alt={`Slide ${index}`}
+                                // className={`slider-image-app-mobile ${index === currentIndex ? 'fade-in' : ''}`}
+                                style={{
+                                    opacity: fade ? 0 : 1,
+                                    transition: 'opacity 2s ease-in-ou',
+                                    position: 'absolute',
+                                    width: '100%',
+                                    zIndex: 1,
+                                }}
+                            />
+                        {/*)}*/}
+                    </div>
+
+                    <div className='slider-background-app-mobile'>
+                        <p className='slider-text-app-mobile-p-scrollable'>
+                            Od 2020 roku prowadzimy Studio Estetyki w którym zapewniamy Państwu najlepsze usługi,
+                            połączenie wiedzy, indywidualnego podejścia, a przede wszystkim bezpieczeństwa
+                            wykonywanych
+                            zabiegów. Nie wykonujemy „zabiegów na życzenie”, nie będących w wachlarzu naszych usług.
+                            Ofertę
+                            jaką Państwo przedstawiamy, zbudowaliśmy na bazie wielogodzinnych szkoleń, rozmów,
+                            dobierania
+                            sprzętu i testowania go na długo przed zakupem. Posiadamy sprzęt najwyżej klasy, tylko
+                            sprawdzonych producentów, gwarantujących swoją marką i doświadczeniem najwyższe
+                            parametry.
+                            Stosujemy zasadę opieki nad klientem w trakcie całego procesu zabiegowego.
+                            Będzie nam niezmiernie miło spotkać się z Państwem w naszych gabinetach i wspólnie iść
+                            drogą
+                            piękna, higieny i zdrowego życia!
+                        </p>
+
+                        {/*<div className="dots-container-app">*/}
+                        {/*    {photos.map((_, index) => (*/}
+                        {/*        <span*/}
+                        {/*            key={index}*/}
+                        {/*            className={`dot-app ${index === currentIndex ? 'active' : ''}`}*/}
+                        {/*            onClick={() => setCurrentIndex(index)}*/}
+                        {/*        />*/}
+                        {/*    ))}*/}
+                        {/*</div>*/}
+                    </div>
 
 
                 </div>
-
-                <div className='slider-background-app-mobile'>
-                    <p className='slider-text-app-mobile-p-scrollable'>
-                        Od 2020 roku prowadzimy Studio Estetyki w którym zapewniamy Państwu najlepsze usługi,
-                        połączenie wiedzy, indywidualnego podejścia, a przede wszystkim bezpieczeństwa
-                        wykonywanych
-                        zabiegów. Nie wykonujemy „zabiegów na życzenie”, nie będących w wachlarzu naszych usług.
-                        Ofertę
-                        jaką Państwo przedstawiamy, zbudowaliśmy na bazie wielogodzinnych szkoleń, rozmów,
-                        dobierania
-                        sprzętu i testowania go na długo przed zakupem. Posiadamy sprzęt najwyżej klasy, tylko
-                        sprawdzonych producentów, gwarantujących swoją marką i doświadczeniem najwyższe
-                        parametry.
-                        Stosujemy zasadę opieki nad klientem w trakcie całego procesu zabiegowego.
-                        Będzie nam niezmiernie miło spotkać się z Państwem w naszych gabinetach i wspólnie iść
-                        drogą
-                        piękna, higieny i zdrowego życia!
-                    </p>
-
-                    {/*<div className="dots-container-app">*/}
-                    {/*    {photos.map((_, index) => (*/}
-                    {/*        <span*/}
-                    {/*            key={index}*/}
-                    {/*            className={`dot-app ${index === currentIndex ? 'active' : ''}`}*/}
-                    {/*            onClick={() => setCurrentIndex(index)}*/}
-                    {/*        />*/}
-                    {/*    ))}*/}
-                    {/*</div>*/}
-                </div>
+            </Col>
+        );
+    }
 
 
-            </div>
-        </Col>
-    );
-}
